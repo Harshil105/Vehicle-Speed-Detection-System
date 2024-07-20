d4443833fa28fc63ca42576fdce612e2df886aa0
Harshil105 <140583584+Harshil105@users.noreply.github.com>
2024-07-21 00:04:04 +0530

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,5 +1,5 @@
-account_sid = 'AC97121bd48600c6ca9e1bdc1168fa976b'
-auth_token = 'da5d584ee7f1c0a6f9c75f1b2c0edff2'
+account_sid = ''
+auth_token = ''
 
-twilio_number = '+12543313251'
-my_phone_number = '+919145267582'
+twilio_number = ''
+my_phone_number = ''
